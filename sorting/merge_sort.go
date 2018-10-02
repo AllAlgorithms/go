355cca46e5193e522bd5318921fe0cb4f90a7a76
Ton <chayakorn.ton@gmail.com>
2018-10-02 12:28:15 +0700

    Merge Sort

---
+++ sorting/merge_sort.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	slice := []int{12, 11, 13, 5, 6, 7}
+	fmt.Println("Given array is \n\n", slice)
+	fmt.Println("Sorted array is \n\n", MergeSort(slice), "\n")
+}
+
+func MergeSort(slice []int) []int {
+
+	if len(slice) < 2 {
+		return slice
+	}
+	mid := (len(slice)) / 2
+	return Merge(MergeSort(slice[:mid]), MergeSort(slice[mid:]))
+}
+
+func Merge(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		}
+	}
+	return slice
+}
